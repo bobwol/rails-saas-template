d1784009ff9503e05d2621c5558f6807b2d2b8be
Rich Buggy <rich@buggy.id.au>
2014-09-01 17:50:18 +1000

    Use FactoryGirl to lint all of the factories on startup

---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,14 @@
+# RSpec
+# spec/support/factory_girl.rb
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+end
